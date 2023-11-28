92db5db2c09bf4e7c039464ec077fec48468d8ed
ayoWhatItDo <130956867+ayoWhatItDo@users.noreply.github.com>
2023-11-27 18:43:17 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import JokeJokes from "./JokeJokes";
+
+function App() {
+  return (
+    <div className="App">
+      <JokeJokes />
+    </div>
+  );
+}
+
+export default App;
